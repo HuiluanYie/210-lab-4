4e88de1b05509d0c4359d47dd0d7065559dcba9d
HuiluanYie <huiluanyie@gmail.com>
2026-09-04 22:49:43 -0700

    Initial commit

--- main.cpp
+++ main.cpp
@@ -1,31 +1,13 @@
 // COMSC-210 | Lab 4 | Huiluan Yie
 
 #include <iostream>
-#include <vector>
 using namespace std;
 
-struct Color {
-    int red, green, blue;
-};
-
 //Function prototype
-void outputColor(const Color &);
 
 int main() {
-    //
-    Color c1 = {0, 0, 0};
-    outputColor(c1);
+
     return 0;
 }
 
 //Function definition
-void outputColor(const Color &c) {
-    // outputColor(const Color &c) outputs the Color struct's data in a nice, presentable format
-    // arguments: Color
-    // returns: null
-    cout << "Color RGB values:\n";
-    cout << "Red:   " << c.red << '\n';
-    cout << "Green: " << c.green << '\n';
-    cout << "Blue:  " << c.blue << '\n';
-    cout << endl;
-}
