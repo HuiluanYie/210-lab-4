3eed4a31c3504f870c4e2f3c7d6f145807d9b691
HuiluanYie <huiluanyie@gmail.com>
2026-09-04 23:00:21 -0700

    Commit 1

--- main.cpp
+++ main.cpp
@@ -3,11 +3,25 @@
 #include <iostream>
 using namespace std;
 
+struct Color {
+    int red, green, blue;
+};
+
 //Function prototype
+void outputColor(const Color );
 
 int main() {
+    Color color = {0, 0, 0};
+    outputColor(color);
 
     return 0;
 }
 
 //Function definition
+void outputColor(const Color &c) {
+    cout << "Color RGB values:\n";
+    cout << "Red:   " << c.red << '\n';
+    cout << "Green: " << c.green << '\n';
+    cout << "Blue:  " << c.blue << '\n';
+    cout << endl;
+}
