8cdea9d435dc97d44ec120e1d92a32088f503ecf
HuiluanYie <huiluanyie@gmail.com>
2026-09-04 23:29:05 -0700

    Commit 3

--- main.cpp
+++ main.cpp
@@ -5,37 +5,53 @@
 using namespace std;
 
 const int N_MIN = 25, N_MAX = 50;
-const int MIN = 25, MAX = 50;
+const int COLOR_MIN = 0, COLOR_MAX = 255;
 
 struct Color {
     int red, green, blue;
 };
 
 //Function prototype
-void outputColor(const Color &);
+//Function prototype
+Color populate_color();
+void output_color(const Color &);
 
 int main() {
     srand(time(0));
-    int n = rand() % (MAX - MIN + 1) + MIN;
+    int n = rand() % (N_MAX - N_MIN + 1) + N_MIN;
     vector<Color> v;
-    Color temp_color;
     int r, g, b;
 
+    // push n random colors into the vector
     for (int i = 0; i < n; i++)
     {
-        r = rand() %
+        r = rand() % (COLOR_MAX - COLOR_MIN + 1) + COLOR_MIN;
+        g = rand() % (COLOR_MAX - COLOR_MIN + 1) + COLOR_MIN;
+        b = rand() % (COLOR_MAX - COLOR_MIN + 1) + COLOR_MIN;
         temp_color = {r, g, b};
         v.push_back(temp_color);
     }
     
-    
-    outputColor(v[0]);
+    for (int i = 0; i < n; i++)
+    {
+        temp_color = {r, g, b};
+        v.push_back(temp_color);
+    }
+    output_color(v[0]);
 
     return 0;
 }
 
 //Function definition
-void outputColor(const Color &c) {
+Color populate_color() {
+    // populate_color() populate the Color struct's data in a nice, presentable format
+    // arguments: Color
+    // returns: null
+    Color temp;
+    return temp;
+}
+
+void output_color(const Color &c) {
     // outputColor(const Color &c) outputs the Color struct's data in a nice, presentable format
     // arguments: Color
     // returns: null
