f09ab9fa75f4d592ab881fca9f0a17d9620f9c62
HuiluanYie <huiluanyie@gmail.com>
2026-09-04 22:39:44 -0700

    Milestone 1

--- main.cpp
+++ main.cpp
@@ -4,12 +4,23 @@
 using namespace std;
 
 struct Color {
-    
+    int red, green, blue;
 };
 
 //Function prototype
+void outputColor(const Color &);
 
 int main() {
-    
+    Color c1 = {0, 0, 0};
+    outputColor(c1);
     return 0;
+}
+
+//Function definition
+void outputColor(const Color &c) {
+    cout << "Color RGB values:\n";
+    cout << "Red:   " << c.red << '\n';
+    cout << "Green: " << c.green << '\n';
+    cout << "Blue:  " << c.blue << '\n';
+    cout << endl;
 }
